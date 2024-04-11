0b1ec4a787e169e7234cc6c1aa5e9ba75b2f9455
fotisPag <fotis.pagonis01@gmail.com>
2024-04-11 15:40:23 +0300

    ArithmeticExceptionDemo

---
+++ ArithmeticExceptionDemo.java
@@ -0,0 +1,23 @@
+package gr.aueb.cf.ch8;
+
+import java.util.Scanner;
+
+/*
+ΘΑ ΚΑΝΟΥΜΕ ΔΙΑΙΡΕΣΗ 5 0 ΘΑ ΜΑΣ ΒΓΑΛΕΙ ERROR
+* Είναι unchecked exception αφού συνέβει κατά τον χρόνο εκτέλεσης
+*
+* */
+
+public class ArithmeticExceptionDemo {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a, b, result;
+        System.out.println("Δώστε δύο αριθμούς για διαίρεση");
+        a = in.nextInt();
+        b = in.nextInt();
+
+        result = a / b;
+
+        System.out.print("Το αποτέλεσμα της διαίρεσης είναι: " + result);
+    }
+}
