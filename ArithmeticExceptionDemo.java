8971ba6efbd6fc794c69c0e03efb3a1097641e1e
fotisPag <fotis.pagonis01@gmail.com>
2024-04-11 16:37:44 +0300

    printStackTrace

--- ArithmeticExceptionDemo.java
+++ ArithmeticExceptionDemo.java
@@ -12,12 +12,18 @@ public class ArithmeticExceptionDemo {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int a, b, result;
-        System.out.println("Δώστε δύο αριθμούς για διαίρεση");
-        a = in.nextInt();
-        b = in.nextInt();
 
-        result = a / b;
+       try {
+           System.out.println("Δώστε δύο αριθμούς για διαίρεση");
+           a = in.nextInt();
+           b = in.nextInt();
 
-        System.out.print("Το αποτέλεσμα της διαίρεσης είναι: " + result);
+           result = a / b;
+
+           System.out.print("Το αποτέλεσμα της διαίρεσης είναι: " + result);
+
+       }catch (ArithmeticException e) {
+           e.printStackTrace();;
+        }
     }
 }
